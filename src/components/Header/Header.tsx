940f82f3b8b2569f0fb28aee44248f4540c09c5f
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2021-02-28 21:22:05 +0300

    add expanded header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { signOut } from "../../store/ac/authActions";
 import './header.scss';
 import { HeaderDispatchProps, HeaderStateProps, HeaderTypes } from '../../types';
+import { Button } from 'reactstrap';
 
 const Header = ({ signOut, userName }: HeaderTypes) => {
+  const [ isHeaderExpanded, setIsHeaderExpanded ] = useState(false);
+  const onToggleHeader = useCallback(() => setIsHeaderExpanded(!isHeaderExpanded), [isHeaderExpanded]);
+  const onSignOut = useCallback(() => {
+    onToggleHeader();
+    setTimeout(signOut, 250);
+  }, [ onToggleHeader, signOut ]);
   return (
-    <div className='header' onClick={signOut}>
-      {userName}
+    <div className={`header ${isHeaderExpanded ? 'expanded-header' : ''}`}>
+      <div className='header-info'>
+        <Button className='header-user-name' onClick={onToggleHeader}>{userName}</Button>
+        {
+          isHeaderExpanded && (
+            <Button
+              className='close-header-btn'
+              color='danger'
+              onClick={onSignOut}
+            >
+              X
+            </Button>
+          )
+        }
+      </div>
     </div>
   );
 };
